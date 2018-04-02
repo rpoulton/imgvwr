cac2903d1617977fd04814c8bcf3f923d12cce74
rpoulton <37986297+rpoulton@users.noreply.github.com>
2018-04-01 20:35:52 -0400

    Add files via upload

---
+++ ImgVwr/ImgVwr/ImgVwr.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace ImgVwr
+{
+    partial class ImgVwr
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImgVwr));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ImageList = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.RightButton = new System.Windows.Forms.Button();
+            this.LeftButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.plusButton = new System.Windows.Forms.Button();
+            this.minusButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Directory";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(67, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(419, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(492, 6);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(75, 23);
+            this.OpenButton.TabIndex = 2;
+            this.OpenButton.Text = "Open";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.ImageList);
+            this.groupBox1.Location = new System.Drawing.Point(15, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(137, 465);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Images";
+            // 
+            // ImageList
+            // 
+            this.ImageList.AllowDrop = true;
+            this.ImageList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.ImageList.FormattingEnabled = true;
+            this.ImageList.HorizontalScrollbar = true;
+            this.ImageList.Location = new System.Drawing.Point(0, 16);
+            this.ImageList.Name = "ImageList";
+            this.ImageList.Size = new System.Drawing.Size(137, 446);
+            this.ImageList.TabIndex = 0;
+            this.ImageList.SelectedIndexChanged += new System.EventHandler(this.ImageList_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.RightButton);
+            this.groupBox2.Controls.Add(this.LeftButton);
+            this.groupBox2.Controls.Add(this.clearButton);
+            this.groupBox2.Controls.Add(this.plusButton);
+            this.groupBox2.Controls.Add(this.minusButton);
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Location = new System.Drawing.Point(170, 39);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(397, 465);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Image Preview";
+            // 
+            // RightButton
+            // 
+            this.RightButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.RightButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RightButton.Location = new System.Drawing.Point(35, 436);
+            this.RightButton.Name = "RightButton";
+            this.RightButton.Size = new System.Drawing.Size(23, 23);
+            this.RightButton.TabIndex = 5;
+            this.RightButton.Text = "R";
+            this.RightButton.UseVisualStyleBackColor = true;
+            this.RightButton.Click += new System.EventHandler(this.RightButton_Click);
+            // 
+            // LeftButton
+            // 
+            this.LeftButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.LeftButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LeftButton.Location = new System.Drawing.Point(6, 436);
+            this.LeftButton.Name = "LeftButton";
+            this.LeftButton.Size = new System.Drawing.Size(23, 23);
+            this.LeftButton.TabIndex = 4;
+            this.LeftButton.Text = "L";
+            this.LeftButton.UseVisualStyleBackColor = true;
+            this.LeftButton.Click += new System.EventHandler(this.LeftButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(304, 436);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(23, 23);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "C";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // plusButton
+            // 
+            this.plusButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.plusButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plusButton.Location = new System.Drawing.Point(333, 436);
+            this.plusButton.Name = "plusButton";
+            this.plusButton.Size = new System.Drawing.Size(23, 23);
+            this.plusButton.TabIndex = 2;
+            this.plusButton.Text = "+";
+            this.plusButton.UseVisualStyleBackColor = true;
+            this.plusButton.Click += new System.EventHandler(this.PlusButton_Click);
+            // 
+            // minusButton
+            // 
+            this.minusButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.minusButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minusButton.Location = new System.Drawing.Point(365, 436);
+            this.minusButton.Name = "minusButton";
+            this.minusButton.Size = new System.Drawing.Size(23, 23);
+            this.minusButton.TabIndex = 1;
+            this.minusButton.Text = "-";
+            this.minusButton.UseVisualStyleBackColor = true;
+            this.minusButton.Click += new System.EventHandler(this.MinusButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(391, 446);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // ImgVwr
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 516);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.OpenButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ImgVwr";
+            this.Text = "ImgVwr";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.ImgVwr_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.ImgVwr_DragEnter);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox ImageList;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button minusButton;
+        private System.Windows.Forms.Button plusButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button RightButton;
+        private System.Windows.Forms.Button LeftButton;
+    }
+}
+
---
+++ ImgVwr/ImgVwr/ImgVwr.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImgVwr
+{
+    public partial class ImgVwr : Form
+    {
+        string currentDir = "C:\\Users\\Ricky\\Pictures";
+        int zoomChange = 0;
+
+        public ImgVwr()
+        {
+            InitializeComponent();
+            string[] args = Environment.GetCommandLineArgs();
+            string argFileName="";
+
+            foreach (string arg in args)
+                if (arg.ToLower().EndsWith(".jpg") || arg.ToLower().EndsWith(".jpeg") || arg.ToLower().EndsWith(".png") || arg.ToLower().EndsWith(".bmp"))
+                {
+                    argFileName = Path.GetFileName(arg);
+                    currentDir = Path.GetDirectoryName(arg);
+                    break;
+                }
+
+            try
+            {
+                newDir();
+                ImageList.SelectedIndex= ImageList.FindString(argFileName);
+            }
+            catch { }
+        }
+
+        private void newDir()
+        {
+            textBox1.Text = currentDir;
+            var dirInfo = new DirectoryInfo(currentDir);
+            var files = dirInfo.GetFiles().Where(c => (c.Extension.Equals(".jpg") || c.Extension.Equals(".jpeg") || c.Extension.Equals(".png") || c.Extension.Equals(".bmp")));
+            ImageList.Items.Clear();
+            foreach (var image in files)
+                ImageList.Items.Add(image.Name);
+            if(ImageList.Items.Count>0)
+                ImageList.SelectedIndex = 0;
+        }
+
+        //load dir
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var fb = new FolderBrowserDialog();
+                if(fb.ShowDialog()==System.Windows.Forms.DialogResult.OK)
+                {
+                    currentDir = fb.SelectedPath; //get selected folder
+                    newDir();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        string imgPath;
+        int freshImg = 0;
+        //load img
+        private void ImageList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                var selectedImage = ImageList.SelectedItems[0].ToString();
+                if(!string.IsNullOrEmpty(selectedImage) && !string.IsNullOrEmpty(currentDir))
+                {
+                    imgPath= Path.Combine(currentDir, selectedImage);
+                    DeleteCurrentImg();
+                    LoadImg();
+                    freshImg = 1;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void DeleteCurrentImg()
+        {
+            if (freshImg == 1)
+                pictureBox1.Image.Dispose();
+        }
+
+        private void LoadImg()
+        {
+            pictureBox1.Image = Image.FromFile(imgPath);
+            //Set the zoomed width and height
+            widthOG = pictureBox1.Image.Width;
+            heightOG = pictureBox1.Image.Height;
+            zoomChange = 0;
+        }
+        int widthOG=0, heightOG=0;
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            if(pictureBox1.Image!=null)
+                ZoomButton_Click(0);
+        }
+
+        private void PlusButton_Click(object sender, EventArgs e)
+        {
+            if(pictureBox1.Image!=null)
+                ZoomButton_Click(1);
+        }
+
+        private void MinusButton_Click(object sender, EventArgs e)
+        {
+            if(pictureBox1.Image!=null)
+                ZoomButton_Click(-1);
+        }
+
+        private void LeftButton_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Image img = pictureBox1.Image;
+                Bitmap bmp = new Bitmap(img, img.Width, img.Height);
+                Graphics g = Graphics.FromImage(bmp);
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                bmp.RotateFlip(RotateFlipType.Rotate270FlipNone);
+
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void RightButton_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Image img = pictureBox1.Image;
+                Bitmap bmp = new Bitmap(img, img.Width, img.Height);
+                Graphics g = Graphics.FromImage(bmp);
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                bmp.RotateFlip(RotateFlipType.Rotate90FlipNone);
+
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void ImgVwr_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] dropped = ((string[])e.Data.GetData(DataFormats.FileDrop, false));
+
+            foreach (string drop in dropped)
+                if (drop.ToLower().EndsWith(".jpg") || drop.ToLower().EndsWith(".jpeg") || drop.ToLower().EndsWith(".png") || drop.ToLower().EndsWith(".bmp"))
+                {
+                    ImageList.Items.Add(drop);
+                    ImageList.SelectedIndex = ImageList.Items.Count-1;
+                }
+        }
+
+        private void ImgVwr_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+                e.Effect = DragDropEffects.All;
+        }
+
+        private Point startingPoint = Point.Empty;
+        private Point movingPoint = Point.Empty;
+        private bool panning = false;
+
+        void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            panning = true;
+            startingPoint = new Point(e.Location.X - movingPoint.X,
+                                      e.Location.Y - movingPoint.Y);
+        }
+
+        void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            panning = false;
+        }
+
+        void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(panning && zoomChange==1)
+            {
+                movingPoint = new Point(e.Location.X - startingPoint.X,
+                                        e.Location.Y - startingPoint.Y);
+                pictureBox1.Invalidate();
+            }
+        }
+
+        void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if(panning && zoomChange==1)
+            {
+               e.Graphics.Clear(Color.White);
+               e.Graphics.DrawImage(pictureBox1.Image, movingPoint);
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                if (Directory.Exists(textBox1.Text))
+                {
+                    currentDir = textBox1.Text;
+                    newDir();
+                    e.Handled=true;
+                }
+        }
+
+        private void ZoomButton_Click(int zoom)
+        {
+            Image img = pictureBox1.Image;
+            //Set the zoomed width and height
+            int imgWidth = pictureBox1.Image.Width;
+            int imgHeight = pictureBox1.Image.Height;
+            if(zoomChange==0)
+            {
+                double imgW2H = Convert.ToDouble(imgWidth) / imgHeight;
+                imgWidth = pictureBox1.Width;
+                imgHeight = Convert.ToInt32( imgWidth / imgW2H);
+            }
+
+            if (zoom!=0)
+            {
+                pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+                int newHeight = Convert.ToInt32(imgHeight + (zoom*.1* imgHeight));
+                int newWidth = Convert.ToInt32(imgWidth + (zoom *.1 * imgWidth));
+
+                Bitmap bmp = new Bitmap(img, newWidth,newHeight);
+                Graphics g = Graphics.FromImage(bmp);
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                pictureBox1.Image = bmp;
+                zoomChange = 1;
+            }
+            else
+            {
+                pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+                Bitmap bmp = new Bitmap(img);
+                Graphics g = Graphics.FromImage(bmp);
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                pictureBox1.Image = bmp;
+                zoomChange = 0;
+            }
+        }
+    }
+}
